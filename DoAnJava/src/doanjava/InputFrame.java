e0b815ecdcb79d9c68629a8d0b520476dab80328
vosivai <37525574+vosivai@users.noreply.github.com>
2018-05-20 16:40:26 +0700

    Update frame INPUT

--- DoAnJava/src/doanjava/InputFrame.java
+++ DoAnJava/src/doanjava/InputFrame.java
@@ -11,6 +11,7 @@ import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
@@ -44,7 +45,7 @@ public class InputFrame extends JFrame
     private int num = 0,frame = 0, Trang = 0;
     private int[] arrays;
     private int[] frames;
-    private JButton btnOK,btRandom,btTaoMang;    
+    private JButton btnOK,btReSet,btTaoMang;    
 
 
     public static void main(String[] args) {
@@ -74,58 +75,50 @@ public class InputFrame extends JFrame
 	contentPane.setLayout(null);
 		
 	JLabel lb1 = new JLabel("Nhập số trang (<10):");
-	lb1.setBounds(10, 10, 200, 25);
+	lb1.setBounds(10, 10, 150, 25);
 	contentPane.add(lb1);
         
-        JLabel lb2 = new JLabel("Nhập số phần tử mảng (<50):");
-	lb2.setBounds(10, 40, 200, 25);
+        JLabel lb2 = new JLabel("Nhập số phần tử mảng (<20):");
+	lb2.setBounds(10, 40, 150, 25);
 	contentPane.add(lb2);
         
-        JLabel lb3 = new JLabel("Nhập số khung trang (<50):");
-	lb3.setBounds(10, 70, 200, 25);
+        JLabel lb3 = new JLabel("Nhập số khung trang (<20):");
+	lb3.setBounds(10, 70, 150, 25);
 	contentPane.add(lb3);
         
-        JLabel lb4 = new JLabel("hfh");
-	//lb4.setBounds(10, 8, 200, 25);
-	//contentPane.add(lb4);
-        
-        JLabel lb5 = new JLabel("hfh");
-	lb5.setBounds(10, 8, 200, 25);
-	//contentPane.add(lb5);
-        
-        SpinnerModel sm = new SpinnerNumberModel(2, 1, 10, 1);
-	spTrang = new JSpinner(sm);
+        SpinnerModel sm1 = new SpinnerNumberModel(2, 1, 10, 1);
+	spTrang = new JSpinner(sm1);
 	JFormattedTextField txt3 = ((JSpinner.NumberEditor) spTrang.getEditor()).getTextField();
 	((NumberFormatter) txt3.getFormatter()).setAllowsInvalid(true);
-	spTrang.setBounds(217, 10, 100, 25);
+	spTrang.setBounds(180, 10, 80, 25);
 	contentPane.add(spTrang);	
 		
-	SpinnerModel sm1 = new SpinnerNumberModel(2, 1, 50, 1);
-	spNum = new JSpinner(sm1);
+	SpinnerModel sm2 = new SpinnerNumberModel(2, 1, 20, 1);
+	spNum = new JSpinner(sm2);
 	JFormattedTextField txt1 = ((JSpinner.NumberEditor) spNum.getEditor()).getTextField();
 	((NumberFormatter) txt1.getFormatter()).setAllowsInvalid(true);
-	spNum.setBounds(217, 40, 100, 25);
+	spNum.setBounds(180, 40, 80, 25);
 	contentPane.add(spNum);	
         
-        SpinnerModel sm2 = new SpinnerNumberModel(2, 1, 50, 1);
-	spFrame = new JSpinner(sm2);
+        SpinnerModel sm3 = new SpinnerNumberModel(2, 1, 20, 1);
+	spFrame = new JSpinner(sm3);
 	JFormattedTextField txt2 = ((JSpinner.NumberEditor) spFrame.getEditor()).getTextField();
 	((NumberFormatter) txt2.getFormatter()).setAllowsInvalid(true);
-	spFrame.setBounds(217, 70, 100, 25);
+	spFrame.setBounds(180, 70, 80, 25);
 	contentPane.add(spFrame);	
         
-        //btRandom = new JButton("Tạo Ngẫu nhiên");
-	//btRandom.setBackground(SystemColor.activeCaption);
-        //btRandom.setBounds(337, 10, 120, 25);
-	//contentPane.add(btRandom);
+        btReSet = new JButton("Đặt lại");
+	btReSet.setBackground(SystemColor.activeCaption);
+        btReSet.setBounds(380, 10, 80, 25);
+	contentPane.add(btReSet);
         
         cbRandom = new JCheckBox("Ngẫu nhiên",false);
-        cbRandom.setBounds(337, 10, 120, 25);
+        cbRandom.setBounds(290, 10, 80, 25);
         contentPane.add(cbRandom);
 		
 	btTaoMang = new JButton("Tạo Mảng");
 	btTaoMang.setBackground(SystemColor.activeCaption);
-        btTaoMang.setBounds(337, 40, 120, 25);
+        btTaoMang.setBounds(290, 40, 170, 25);
 	contentPane.add(btTaoMang);
         
 	btTaoMang.addActionListener(new ActionListener() {
@@ -137,7 +130,7 @@ public class InputFrame extends JFrame
 		
 	btnOK = new JButton("Xác nhận");
         btnOK.setBackground(SystemColor.activeCaption);
-	btnOK.setBounds(337, 70, 120, 25);
+	btnOK.setBounds(290, 70, 170, 25);
 	contentPane.add(btnOK);
 	btnOK.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -156,19 +149,19 @@ public class InputFrame extends JFrame
 		arrays = new int[num];
 		
 		for (int i = 0; i < num; i++) {
-			lbArrays[i] = new JLabel("A[" + i + "]:");
-			SpinnerModel smValue = new SpinnerNumberModel(0, 0, 100, 1);
+			lbArrays[i] = new JLabel("A[" + (i+1) + "]:");
+			SpinnerModel smValue = new SpinnerNumberModel(0, 0, 20, 1);
 			txtArrays[i] = new JSpinner(smValue);
 			JFormattedTextField txt = ((JSpinner.NumberEditor) txtArrays[i].getEditor()).getTextField();
-			((NumberFormatter) txt.getFormatter()).setAllowsInvalid(false);
+			((NumberFormatter) txt.getFormatter()).setAllowsInvalid(true);
 			contentPane.add(lbArrays[i]);
 			contentPane.add(txtArrays[i]);
 			lbArrays[i].setSize(40,30);
-			if (i == 0 || i == 5 || i == 10) 
-				lbArrays[i].setLocation(150 * (i + 1)/5  , 40);
+			if (i == 0 || i == 5 || i == 10 || i == 15) 
+                                lbArrays[i].setLocation(10, 100+ 40*(i+1)/3);
 			else
-				lbArrays[i].setLocation(lbArrays[i-1].getX(), lbArrays[i-1].getY() + 40);
-			txtArrays[i].setSize(50,30);
+				lbArrays[i].setLocation(lbArrays[i-1].getX()+95, lbArrays[i-1].getY());
+			txtArrays[i].setSize(40,30);
 			txtArrays[i].setLocation(lbArrays[i].getX() + 40, lbArrays[i].getY());
 		}
 		contentPane.setVisible(true);
@@ -210,6 +203,27 @@ public class InputFrame extends JFrame
     public int getNum()
     {
 	return num;
+    }
+    
+    // Khoi tao mang cac o nho
+    public void TaoMang() {
+        //
+    }
+
+    // xoa mang o nho
+    public void XoaMang() {
+        //
+    }
+
+    // tao mang o nho random
+    public void TaoRanDomMang(int num, int max) {
+        Random rand = new Random();
+        for (int i = 0; i < num; i++) {
+            int ranNum = rand.nextInt(max) + 0;
+            //lbArrays[i].setText(String.valueOf(ranNum));
+            //lbArrays[i].setForeground(Color.BLUE);
+            //array[i] = ranNum;
+        }
     }
     
     // tra ve so khung trang
